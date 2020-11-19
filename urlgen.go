29542aa3b166caf84b9e5e6f018a796666e5d20d
Tom Myers <tmyers273@gmail.com>
2020-11-19 11:55:45 -0500

    Removes unused code

--- urlgen.go
+++ urlgen.go
@@ -8,10 +8,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"github.com/valyala/fasthttp"
-	"io/ioutil"
-	"net/http"
 	"net/url"
-	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -35,84 +32,6 @@ type AmazonMWSAPI struct {
 	AuthToken     string
 	MarketplaceId string
 	SellerId      string
-}
-
-func (api AmazonMWSAPI) genSignAndFetch(Action string, ActionPath string, Parameters map[string]string) (string, error) {
-	genUrl, err := GenerateAmazonUrl(api, Action, ActionPath, Parameters)
-	if err != nil {
-		return "", err
-	}
-
-	SetTimestamp(genUrl)
-
-	signedurl, err := SignAmazonUrl(genUrl, api)
-	if err != nil {
-		return "", err
-	}
-
-	resp, err := http.Get(signedurl)
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func (api AmazonMWSAPI) genSignAndFetchViaPost(Action string, ActionPath string, Parameters map[string]string) (string, error) {
-	if api.AuthToken != "" {
-		Parameters["MWSAuthToken"] = api.AuthToken
-	}
-
-	Parameters["Action"] = Action
-	Parameters["AWSAccessKeyId"] = api.AccessKey
-	Parameters["SellerId"] = api.SellerId
-	Parameters["SignatureVersion"] = "2"
-	Parameters["SignatureMethod"] = "HmacSHA256"
-	Parameters["Version"] = "2011-10-01"
-	Parameters["Timestamp"] = time.Now().UTC().Format(time.RFC3339)
-
-	genUrl, err := GenerateAmazonUrlPost(api, ActionPath)
-	if err != nil {
-		return "", err
-	}
-
-	signature, err := sign("POST", genUrl, Parameters, api)
-	if err != nil {
-		return "", err
-	}
-	Parameters["Signature"] = signature
-
-	v := url.Values{}
-	for index, value := range Parameters {
-		v.Set(index, value)
-	}
-	s := v.Encode()
-
-	req, err := http.NewRequest("POST", genUrl.String(), strings.NewReader(s))
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
 }
 
 var strPost = []byte("POST")
@@ -188,49 +107,6 @@ func GenerateAmazonUrlPost(api AmazonMWSAPI, ActionPath string) (finalUrl *url.U
 	result.Host = api.Host
 	result.Scheme = "https"
 	result.Path = ActionPath
-
-	return result, nil
-}
-
-func GenerateAmazonUrl(api AmazonMWSAPI, Action string, ActionPath string, Parameters map[string]string) (finalUrl *url.URL, err error) {
-	result, err := url.Parse(api.Host)
-	if err != nil {
-		return nil, err
-	}
-
-	result.Host = api.Host
-	result.Scheme = "https"
-	result.Path = ActionPath
-
-	var version string
-	re := regexp.MustCompile(`/\w+/(\d{4}-\d{2}-\d{2})`)
-	match := re.FindStringSubmatch(ActionPath)
-
-	if len(match) == 2 {
-		version = match[1]
-	} else {
-		version = "2011-10-01"
-	}
-
-	values := url.Values{}
-	values.Add("Action", Action)
-
-	if api.AuthToken != "" {
-		values.Add("MWSAuthToken", api.AuthToken)
-	}
-
-	values.Add("AWSAccessKeyId", api.AccessKey)
-	values.Add("SellerId", api.SellerId)
-	values.Add("SignatureVersion", "2")
-	values.Add("SignatureMethod", "HmacSHA256")
-	values.Add("Version", version)
-
-	for k, v := range Parameters {
-		values.Set(k, v)
-	}
-
-	params := values.Encode()
-	result.RawQuery = params
 
 	return result, nil
 }
